34731a48e5f5955d8bf5eb3335a700affaaff8c1
Kristoffer <kristoffer.kallerup@outlook.com>
2021-02-16 16:05:59 +0100

    Add vuetify

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,11 +1,11 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorldVue from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HelloWorldVue, {
       propsData: { msg },
     });
     expect(wrapper.text()).to.include(msg);
